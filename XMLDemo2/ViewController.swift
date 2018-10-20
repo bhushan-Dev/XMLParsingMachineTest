e3a3716197e8c595b1df7262955f40051a6b4c2c
bhushan-Dev <bhushan.udawant30@gmail.com>
2018-10-20 14:45:46 +0530

    Initial Commit

---
+++ XMLDemo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMLDemo2
+//
+//  Created by Bhushan Udawant on 20/10/18.
+//  Copyright © 2018 Bhushan Udawant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
